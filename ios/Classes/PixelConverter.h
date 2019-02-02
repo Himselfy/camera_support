8e5a08cf0c3cae5c94b4b525b37773729e52203e
Developer Taxxo <developertaxxo@Developers-Mac.local>
2019-02-02 11:11:33 -0800

    objc pixelbuffer convert

---
+++ ios/Classes/PixelConverter.h
@@ -0,0 +1,12 @@
+//
+//  PixelConverter.h
+//  camera_support
+//
+//  Created by Developer Taxxo on 31/01/2019.
+//
+
+#ifndef PixelConverter_h
+#define PixelConverter_h
+
+
+#endif /* PixelConverter_h */
---
+++ ios/Classes/PixelConverter.m
@@ -0,0 +1,8 @@
+//
+//  PixelConverter.m
+//  camera_support
+//
+//  Created by Developer Taxxo on 31/01/2019.
+//
+
+#import <Foundation/Foundation.h>
